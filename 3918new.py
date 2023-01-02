7ad04612c7ff2fe0cf06974c0f16a3fdf44b8450
billdarkest <60397484+billdarkest@users.noreply.github.com>
2023-01-02 13:53:12 +0100

    Update 3918new.py

--- 3918new.py
+++ 3918new.py
@@ -48,7 +48,27 @@ def callback():
     # if event is MessageEvent and message is TextMessage, then echo text
     for event in events:
         if len(event.message.text) == 4:
-            line_bot_api.reply_message(event.reply_token, TextSendMessage(text='維修中'))
+            headers = {
+            "User-Agent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Mobile Safari/537.36"}
+            res = requests.get(
+            "https://docs.google.com/spreadsheets/d/e/2PACX-1vTfHqhAoeOGajlma3K7Ym1CngD2VI3ua99fwPc767QpExzAMyV81S6L1IZ6TwzSPLO2irkZt96QA-3h/pubhtml",
+            headers=headers)
+            DG = re.findall('</div></th><td class="s0" dir="ltr">(.*?)</td><td class=', res.content.decode('utf-8'), re.S)
+            SH = re.findall(
+            '<div class="softmerge-inner" style="width:27px;left:-1px">(.*?)</div>',
+            res.content.decode('utf-8'), re.S)
+            EMS = re.findall('/td><td class="s0" dir="ltr">(.*?)</td><',
+                         res.content.decode('utf-8'), re.S)
+            SS = re.findall('QQQ(.*?)AAA<', res.content.decode('utf-8'), re.S)
+            DG.remove('un_no')
+            SH.remove('proper_shipping_name')
+            EMS.remove('ems')
+            SS.remove('stowage_and_segregation')
+            for D in range(0, 2853):
+                if event.message.text == DG[D]:
+                    line_bot_api.reply_message(event.reply_token, TextSendMessage(text="這是" + SH[D] + ",ems為" + EMS[D]))
+                    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=SS[D]))
+
     return 'OK'
 
 
