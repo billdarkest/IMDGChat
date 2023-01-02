a14f32d2947f35fe63fc657a16991ab59668e85d
billdarkest <60397484+billdarkest@users.noreply.github.com>
2023-01-02 13:39:59 +0100

    Update 3918new.py

--- 3918new.py
+++ 3918new.py
@@ -47,8 +47,8 @@ def callback():
 
     # if event is MessageEvent and message is TextMessage, then echo text
     for event in events:
-        if len(event) == 4
-        line_bot_api.reply_message(event.reply_token,TextSendMessage(text='維修中'))
+        if len(event) == 4:
+            line_bot_api.reply_message(event.reply_token, TextSendMessage(text='維修中'))
     return 'OK'
 
 
