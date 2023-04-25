ead4df3cb666abddc0f08f3f626269b4256f6ca0
billdarkest <60397484+billdarkest@users.noreply.github.com>
2023-04-25 13:55:22 +0200

    Update 3918new.py

--- 3918new.py
+++ 3918new.py
@@ -52,7 +52,7 @@ def handle_message(event):
             targeturl ="這是" + SH[D] + ", Ems為 " + EMS[D] + '\n' + SS[D]
             line_bot_api.reply_message(event.reply_token, TextSendMessage(text=targeturl))
             
-    elif len(event.message.text) == 5 and event.message.text[0] == '*':
+    if len(event.message.text) == 5 and event.message.text[0] == '*':
         targeturl = "https://ss.shipmentlink.com/tvs2/jsp/TVS2_VesselSchedule.jsp?vslCode=" + event.message.text[1:] + "&vslNasme="
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text=targeturl))
     elif len(event.message.text) == 5 and event.message.text[0] == '+':
